f5481b21af63e44296cb33f63b96c4ce1a70bf17
Arwen Ferro <aferro12@terpmail.umd.edu>
2026-03-21 17:49:48 -0400

    arwen did like 10 minutes of work

--- Bloom-Filter/src/Main.java
+++ Bloom-Filter/src/Main.java
@@ -1,13 +1,24 @@
+import  java.util.BitSet;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+}
+
+public class BloomFilter {
+    private int m;
+    private BitSet bit_array;
+    private
+
+    public BloomFilter(int m) {
+        this.m = m;
+        this.bit_array = new BitSet(m);
+    }
+
+    void add(int item) {
+// need the hash function!
     }
 }
