c0a5d714400e3fac1872b78d10f9367892e1854c
Arwen Ferro <aferro12@terpmail.umd.edu>
2026-04-12 22:36:47 -0400

    init

--- Bloom-Filter/src/Main.java
+++ Bloom-Filter/src/Main.java
@@ -1,24 +1,55 @@
 import  java.util.BitSet;
+//import edu.princeton.cs.randomhash.*;
+import com.google.common.hash.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-
+ //todo: test your stuff bro
 }
 
 public class BloomFilter {
-    private int m;
+    private int m; // size of the bit array
     private BitSet bit_array;
-    private
+    private int k; // number of hash functions
+    private List<HashFunction> hashFunctions = new ArrayList<>();
 
-    public BloomFilter(int m) {
-        this.m = m;
+
+    public BloomFilter(int setCount, int falsePosProb) {
+        this.m = getSize(setCount, falsePosProb);
         this.bit_array = new BitSet(m);
+        this.k = getK(m, setCount);
+        for (int i = 0; i <k; i++)  {
+            hashFunctions.add(Hashing.murmur3_32_fixed(i)); // use i as the seed
+        }
+
     }
 
     void add(int item) {
-// need the hash function!
+        for (int i = 0; i < k; i++) {
+            int digest = hashFunctions.get(i).hashInt(i).asInt() % m; // mod m to make it fit in the filter
+            bit_array.set(digest, true); // set that index to a 1
+        }
+    }
+
+    boolean check(int item) {
+        for (int i = 0; i < k; i++) {
+            int digest = hashFunctions.get(i).hashInt(i).asInt() % m; // mod m to make it fit in the filter
+            if (bit_array.get(digest) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int getSize(int elems, int fp) {
+        double doubleFp = fp;
+        int m = (int) Math.ceil(elems * Math.log(doubleFp) / (Math.log(2)*Math.log(2)));
+        return m;
+    }
+
+    int getK(int m, int n) {
+        int k = (int)Math.ceil(((double)m/(double)n) * Math.log(2));
+        return k;
     }
 }
